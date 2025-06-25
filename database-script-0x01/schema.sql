5f1962a1d7c6ce0fe26e83db836ecd1b87bfa54c
Khalil Hafz <129151938+Hafz-technology@users.noreply.github.com>
2025-06-25 22:28:05 +0300

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,13 +1,14 @@
--- # Entities and Attributes
--- ## User
--- * user_id: Primary Key, UUID, Indexed
--- * first_name: VARCHAR, NOT NULL
+/* # Entities and Attributes
+## User
+* user_id: Primary Key, UUID, Indexed
+* first_name: VARCHAR, NOT NULL
 * last_name: VARCHAR, NOT NULL
 * email: VARCHAR, UNIQUE, NOT NULL
 * password_hash: VARCHAR, NOT NULL
 * phone_number: VARCHAR, NULL
 * role: ENUM (guest, host, admin), NOT NULL
-* created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+* created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP   */
+
 ## Property
 * property_id: Primary Key, UUID, Indexed
 * host_id: Foreign Key, references User(user_id)
